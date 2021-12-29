38dc935919ccbca572343fd9531f242db8e521a5
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-12-29 21:14:01 +0100

    Create About.vue

---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-main>
+    <v-container>
+      <Navbar />
+      <div class="about">
+        <h1>About</h1>
+      </div>
+    </v-container>
+    <Footer />
+  </v-main>
+</template>
+<script>
+import Footer from "../components/warpper/Footer";
+import Navbar from "../components/warpper/Navbar";
+
+export default {
+  name: "Dashboard",
+
+  components: {
+    Footer,
+    Navbar,
+  },
+
+  data: () => ({}),
+};
+</script>
