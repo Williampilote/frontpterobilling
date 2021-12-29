1a47e73fb246fc21f39cd45a0846d23cb2d75f38
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-12-29 21:13:01 +0100

    Create Tabs.vue

---
+++ src/components/Server/Tabs.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-sheet elevation="6">
+    <v-tabs
+      background-color="#243242"
+      dark
+      next-icon="mdi-arrow-right-bold-box-outline"
+      prev-icon="mdi-arrow-left-bold-box-outline"
+      show-arrows
+    >
+      <v-tabs-slider color="yellow"></v-tabs-slider>
+      <v-tab v-for="link in router" :key="link.name" :href="link.path">
+        {{ link.name }}
+      </v-tab>
+    </v-tabs>
+  </v-sheet>
+</template>
+
+<script>
+import { router } from "../../router/index.js";
+
+export default {
+  name: "Tabs",
+
+  components: {},
+
+  data: () => ({
+    name: [],
+    router: [
+      { name: "Console", path: router.currentRoute.path + "./" },
+      { name: "Files", path: router.currentRoute.path + "/files" },
+    ],
+  }),
+  methods: {},
+};
+</script>
