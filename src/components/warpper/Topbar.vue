99f98330f099b9bc3df12131f77fd1661fc15803
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-12-29 21:13:13 +0100

    Create Topbar.vue

---
+++ src/components/warpper/Topbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar app color="#2962ff" dark>
+    <v-spacer> </v-spacer>
+
+    <v-avatar>
+      <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+    </v-avatar>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Topbar",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
