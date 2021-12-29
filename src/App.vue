4a87c18e34f3b34b000484cbe0278fd6acc604d7
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-12-29 21:11:48 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-app id="body">
+      <Topbar />
+      <router-view />
+    </v-app>
+  </div>
+</template>
+
+<style lang="scss">
+#body {
+  font-family: "IBM Plex Sans", Roboto, system-ui, sans-serif;
+
+  font-weight: 500;
+  letter-spacing: 0em;
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+
+<script>
+import Topbar from "./components/warpper/Topbar";
+
+export default {
+  name: "App",
+
+  components: {
+    Topbar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
