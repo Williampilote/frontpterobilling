7a813f503fde7631423e96c9bd278f0fb0fb2831
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-12-29 21:14:13 +0100

    Create Server.vue

---
+++ src/views/Server.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-main style="background-color: #2c3b4c">
+    <Navbar />
+    <Console />
+    <Footer />
+  </v-main>
+</template>
+
+<script>
+import Footer from "../components/warpper/Footer";
+import Navbar from "../components/warpper/Navbar";
+import Console from "../components/Server/Console.vue";
+
+export default {
+  name: "Server",
+
+  components: {
+    Footer,
+    Navbar,
+    Console,
+  },
+
+  data: () => ({}),
+  methods: {},
+};
+</script>
