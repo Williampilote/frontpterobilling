f9b60f1cdfe786d457123adf01e753d0a583f876
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-12-29 21:14:05 +0100

    Create Dashboard.vue

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-main style="background-color: #2c3b4c">
+    <v-list-item>
+      <h2 class="white--text pt-6">{{ name }}</h2>
+    </v-list-item>
+    <Navbar />
+    <ServerRow />
+    <Footer />
+  </v-main>
+</template>
+
+<script>
+import Footer from "../components/warpper/Footer";
+import Navbar from "../components/warpper/Navbar";
+import ServerRow from "../components/Dashboard/ServerRow.vue";
+import { router } from "../router/index.js";
+
+export default {
+  name: "Dashboard",
+
+  components: {
+    Footer,
+    Navbar,
+    ServerRow,
+  },
+
+  data: () => ({
+    name: [],
+  }),
+  methods: {
+    async getRoute() {
+      let namerouter = router.currentRoute.name;
+      this.name = namerouter;
+    },
+  },
+  beforeMount() {
+    this.getRoute();
+  },
+};
+</script>
