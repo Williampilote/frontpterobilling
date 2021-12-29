b4ee958712b818cb1d72baf3b16ca2ef34f8ea2f
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-12-29 21:14:08 +0100

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-main>
+    <Navbar />
+    <v-container fluid>
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+    </v-container>
+    <Footer />
+  </v-main>
+</template>
+
+<script>
+import Footer from "../components/warpper/Footer";
+import Navbar from "../components/warpper/Navbar";
+
+export default {
+  name: "Home",
+
+  components: {
+    Footer,
+    Navbar,
+  },
+
+  data: () => ({}),
+};
+</script>
