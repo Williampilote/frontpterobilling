748ecbe24acf59b88842dcd1c19640fcdc683972
Williampilote <57291065+Williampilote@users.noreply.github.com>
2021-12-29 21:13:07 +0100

    Create Footer.vue

---
+++ src/components/warpper/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-footer padless absolute dark>
+    <v-card flat tile width="100%" class="flex">
+      <v-card-text
+        class="mt-4 white--text pt-0 justify-center d-flex align-center"
+      >
+        All Rights Reserved by Sorexproject. Designed and Developed by
+        Williampilote.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="py-2 white--text text-center" background-color="back">
+        2021 - {{ new Date().getFullYear() }} — <strong>Sorexproject@</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data: () => ({
+    //
+  }),
+};
+</script>
